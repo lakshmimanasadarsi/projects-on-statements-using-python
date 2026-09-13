901fe3ce36064d3857c2190614cd6dc422913666
lakshmimanasadarsi <lakshmimanasadarsi@gmail.com>
2026-09-13 12:10:27 +0530

    Array Statistics Analyser

--- 3.py
+++ 3.py
@@ -1,23 +1,24 @@
+#------array statistics analyser------
 from array import array
-numbers = array('i') # Get number of elements 
-n = int(input("Enter number of elements: ")) # Insert elements into the array 
+numbers = array('i') 
+n = int(input("Enter number of elements: "))
 for i in range(n): 
     value = int(input(f"Enter element {i + 1}: ")) 
     numbers.append(value) 
-print("\nArray:", numbers) # Find largest element 
+print("\nArray:", numbers)  
 largest = numbers[0] 
 for i in range(1, n): 
         if numbers[i] > largest: 
-            largest = numbers[i] # Find smallest element 
+            largest = numbers[i] 
 smallest = numbers[0] 
 for i in range(1, n): 
     if numbers[i] < smallest: 
-        smallest = numbers[i] # Find sum 
+        smallest = numbers[i] 
 total = 0 
 for i in range(n): 
-    total = total + numbers[i] # Find average 
-average = total / n # Display results 
-print("\n----- Array Statistics -----") 
+    total = total + numbers[i] 
+average = total / n  
+print("\n===== Array Statistics =====") 
 print("Largest:", largest) 
 print("Smallest:", smallest) 
 print("Sum:", total) 
